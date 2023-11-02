56018ef5753530642a4a52041e212c63b60a34c0
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-02 14:31:49 +0530

    Created Basic Login Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import netflixLogo from "../utils/Images/Netflix_Logo.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='w-44 bg-gradient-to-b from-black'>
+        <img src={netflixLogo} alt="NetflixLogo"/>
+    </div>
   )
 }
 
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import loginpageBgImage from "../utils/Images/loginPageBgImage.jpg"
+import Header from './Header'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div>
+        {/* Logo */}
+        <div className='absolute'>
+            <Header/>
+        </div>
+        {/* Backgorund Image */}
+        <div>
+            <img src={loginpageBgImage}></img>
+        </div>
+    </div>
   )
 }
 
