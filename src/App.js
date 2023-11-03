bc319f620343df90989b421c6a9c35f42c5a8171
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-03 16:40:55 +0530

    Added Auth Event Listener

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Body from './components/Body';
+import { Provider } from "react-redux";
+import "./App.css";
+import Body from "./components/Body";
+import AppStore from "./utils/AppStore";
 
 function App() {
   return (
     <div>
-      <Body/>
+      <Provider store={AppStore}>
+        <Body />
+      </Provider>
     </div>
   );
 }
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import LoginPage from './LoginPage'
-import BrowsePage from './BrowsePage'
+import React, { useEffect } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import BrowsePage from "./BrowsePage";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/userSlice";
 
-const router=createBrowserRouter([
-    {
-        path:'/',
-        element:<LoginPage/>
-    },
-    {
-        path:'/browse',
-        element:<BrowsePage/>
-    }
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/browse",
+    element: <BrowsePage />,
+  },
+]);
 
 const Body = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // Auth Listener should be called only once so placed in useEffect.
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // SignIn / SignUp
+        const uid = user.uid;
+      } else {
+        // SignOut
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <div>
-        <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
