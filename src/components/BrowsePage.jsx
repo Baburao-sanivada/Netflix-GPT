4f24ce3adc3dc9283f711b33b9f087e9c07aece7
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-06 21:47:02 +0530

    Temp

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 
+// Should be allowed to browse page on when Login/signUp is Successful
 const BrowsePage = () => {
   return (
     <div className="">
