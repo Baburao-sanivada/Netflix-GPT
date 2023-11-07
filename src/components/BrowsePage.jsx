5e197e5d2f1afe3dbb51139e5a8b8ad86c49cd21
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-07 12:24:01 +0530

    Created useNowPlayingMoviesHook

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -1,27 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./Header";
-import { API_Options } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 // Should be allowed to browse page on when Login/signUp is Successful
 const BrowsePage = () => {
-  const dispatch = useDispatch();
-
-  const NowPlayList = async () => {
-    const fetchData = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_Options
-    );
-    const jsonData = await fetchData.json();
-    console.log(jsonData?.results);
-    dispatch(addNowPlayingMovies(jsonData?.results));
-  };
-
-  useEffect(() => {
-    NowPlayList();
-  }, []);
-
+  useNowPlayingMovies();
   return (
     <div className="">
       <Header />
---
+++ src/hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux";
+import { API_Options } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+
+  const NowPlayList = async () => {
+    const fetchData = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_Options
+    );
+    const jsonData = await fetchData.json();
+    console.log(jsonData?.results);
+    dispatch(addNowPlayingMovies(jsonData?.results));
+  };
+
+  useEffect(() => {
+    NowPlayList();
+  }, []);
+};
+
+export default useNowPlayingMovies;
