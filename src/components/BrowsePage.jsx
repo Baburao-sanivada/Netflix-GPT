92066cb5036798b0e612a7c4a94593bb0dc2b306
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-07 12:14:37 +0530

    Created Movies Slice

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_Options } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 // Should be allowed to browse page on when Login/signUp is Successful
 const BrowsePage = () => {
+  const dispatch = useDispatch();
+
   const NowPlayList = async () => {
     const fetchData = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
@@ -11,6 +15,7 @@ const BrowsePage = () => {
     );
     const jsonData = await fetchData.json();
     console.log(jsonData?.results);
+    dispatch(addNowPlayingMovies(jsonData?.results));
   };
 
   useEffect(() => {
