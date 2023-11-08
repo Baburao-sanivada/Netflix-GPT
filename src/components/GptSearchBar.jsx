b9312ef2396426ef70944c18dfe7289d56d868c2
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-08 22:39:33 +0530

    worked on GPTSearch

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const GptSearchBar = () => {
-  return <div>GptSearchBar</div>;
+  return (
+    <div className="pt-[10%] flex justify-center">
+      <form className="bg-black w-1/2 grid grid-cols-12 px-2 py-2">
+        <input
+          type="text"
+          placeholder="What would you like to watch today"
+          className="col-span-9 m-2 px-2 rounded-md"
+        />
+        <button className="py-2 px-4 bg-red-700 col-span-3 m-2 text-white rounded-lg">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default GptSearchBar;
--- src/components/GptSearchPage.jsx
+++ src/components/GptSearchPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import GptSearchBar from "./GptSearchBar";
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import loginpageBgImage from "../utils/Images/loginPageBgImage.jpg";
 
 const GptSearchPage = () => {
-  return <div>GptSearch</div>;
+  return (
+    <div>
+      {/* Backgorund Image */}
+      <div className="absolute -z-10">
+        <img src={loginpageBgImage} alt="BackGorund Image"></img>
+      </div>
+      <GptSearchBar />
+      <GptMovieSuggestions />
+    </div>
+  );
 };
 
 export default GptSearchPage;
