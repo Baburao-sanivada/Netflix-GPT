c5e81050f95f0339007b0ceb285049de67b641f8
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-22 22:15:15 +0530

    Update key

--- src/utils/openAI.jsx
+++ src/utils/openAI.jsx
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-
+// Need to work on how to hide API key
 const openai = new OpenAI({
   apiKey: process.env.REACT_APP_Open_API_Key, // defaults to process.env["OPENAI_API_KEY"]
   dangerouslyAllowBrowser: true,
